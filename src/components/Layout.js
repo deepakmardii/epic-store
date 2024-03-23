3d6767613761bb82551e8540e0c9d9c327076589
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-23 16:37:39 +0530

    Cleanup and layout added

---
+++ src/components/Layout.js
@@ -0,0 +1,9 @@
+export default function AppLayout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header>header</header>
+      <main className="flex-grow bg-[#f7f7f7]">{children}</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
