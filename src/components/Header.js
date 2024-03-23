c4d498371d2a302fa741fde5c826fd6b7afd697b
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-23 16:51:27 +0530

    Logo component added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header className="sticky top-0 bg-white z-10 shadow">
-      <div className="container mx-auto p-6 flex justify-between"></div>
+      <div className="container mx-auto p-6 flex justify-between">
+        <Logo />
+      </div>
     </header>
   );
 }
