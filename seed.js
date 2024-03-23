5919e715fb058e6f9af0e4dbb48e3231cfe6dcd1
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-23 20:59:57 +0530

    fetching necessary data from the server

--- seed.js
+++ seed.js
@@ -1,9 +1,7 @@
 const Stripe = require("stripe");
 const products = require("./products");
 
-const stripe = Stripe(
-  "sk_test_51OxTPgSEfxgOCLvn7OwRjCEyCwxlNfUPWGG0LlR2pO8wKVKDK6oAxo72GiIaoZiydd0EJUGZVeUNRhgBJ6bt83oV00QfvTtMwx"
-);
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 (async () => {
   for (const product of products) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
+import { stripe } from "@/utils/stripe";
+
+export default function Home({ products }) {
+  console.log(products);
   return <div>Home</div>;
+}
+
+export async function getStaticProps() {
+  const inventory = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 8,
+  });
+  const products = inventory.data.map((product) => {
+    const price = product.default_price;
+    return {
+      currench: price.currency,
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount,
+      image: product.images[0],
+    };
+  });
+  return {
+    props: {
+      // products: products => since both prop and value are same with object shorthand we can only write products
+      products,
+    },
+  };
 }
