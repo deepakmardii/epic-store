e82ddb454121a269bd5a8dcedbd0563becb77e24
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-23 17:39:27 +0530

    Metadata for title and favicon

---
+++ src/components/Meta.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Meta() {
+  return (
+    <Head>
+      <title>EpicStore</title>
+      <link rel="icon" type="image/x-ico" href="/favicon.ico" />
+    </Head>
+  );
+}
