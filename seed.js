ef0fde5b4fd3972b67c92c4490215a7beb3c05b3
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-23 18:21:05 +0530

    seed data via stripe API to stripe products page
    node seed.js

--- seed.js
+++ seed.js
@@ -4,3 +4,17 @@ const products = require("./products");
 const stripe = Stripe(
   "sk_test_51OxTPgSEfxgOCLvn7OwRjCEyCwxlNfUPWGG0LlR2pO8wKVKDK6oAxo72GiIaoZiydd0EJUGZVeUNRhgBJ6bt83oV00QfvTtMwx"
 );
+
+(async () => {
+  for (const product of products) {
+    const stripeProduct = await stripe.products.create({
+      name: product.name,
+      default_price_data: {
+        currency: product.currency,
+        unit_amount_decimal: product.price,
+      },
+      images: [product.image],
+    });
+    console.log(stripeProduct.name, ":", stripeProduct.id);
+  }
+})();
