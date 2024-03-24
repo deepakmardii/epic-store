7f929217fac7b7f383c8554371b6fde7c8086006
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-24 20:30:09 +0530

    checkout functionality with stripe

--- src/pages/api/checkout-sessions/index.js
+++ src/pages/api/checkout-sessions/index.js
@@ -4,6 +4,7 @@ import { validateCartItems } from "use-shopping-cart/utilities";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
+      console.log("Hello");
       const cartDetails = req.body;
       const inventory = await stripe.products.list({
         expand: ["data.default_price"],
@@ -14,12 +15,23 @@ export default async function handler(req, res) {
           currency: price.currency,
           id: product.id,
           name: product.name,
-          price: price.unit_ammoutn,
-          image: product.image[0],
+          price: price.unit_amount,
+          image: product.images[0],
         };
       });
       const lineItems = validateCartItems(products, cartDetails);
-    } catch (error) {}
+      const session = await stripe.checkout.sessions.create({
+        mode: "payment",
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/cart`,
+      });
+      res.status(200).json(session);
+    } catch (error) {
+      console.log("Error", error);
+      res.status(500).json({ statusCode: 500, message: error.message });
+    }
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
