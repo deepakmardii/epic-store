60c5f1da4bce2cc37f0e8da22163804b5a4859f4
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-24 17:59:11 +0530

    Shopping cart with increment and decrement item functionality

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,9 +1,12 @@
+import CartProduct from "@/components/CartProduct";
 import Link from "next/link";
 import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
 
 export default function Cartpage() {
-  const { cartCount, clearCart } = useShoppingCart();
+  const { cartCount, clearCart, formattedTotalPrice, cartDetails } =
+    useShoppingCart();
+  console.log(cartDetails);
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       {cartCount > 0 ? (
@@ -31,6 +34,23 @@ export default function Cartpage() {
             </Link>
           </p>
         </>
+      )}
+
+      {cartCount > 0 && (
+        <div className="mt-12 space-y-4">
+          {Object.entries(cartDetails).map(([productId, product]) => (
+            <CartProduct key={productId} product={product} />
+          ))}
+          <div className="flex flex-col items-end border-t py-4 mt-8">
+            <p className="text-xl">
+              Total:
+              <span className="font-semibold">{formattedTotalPrice}</span>
+            </p>
+            <button className="border roounded py-2 px-6 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 focus:ring-opacity-50 focus:ring-yellow-500 text-white transition-colors disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-yellow-500 mt-4 max-w-max rounded">
+              Go to Checkout
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
