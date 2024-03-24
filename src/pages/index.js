81c4f6a1fe1e1a993d1ed1edb09048b8b4c7b572
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-24 09:29:55 +0530

    currency typo fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import ProductCard from "@/components/ProductCard";
 import { stripe } from "@/utils/stripe";
 
 export default function Home({ products }) {
+  console.log(products);
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       <div className="grid gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-1">
@@ -21,7 +22,7 @@ export async function getStaticProps() {
   const products = inventory.data.map((product) => {
     const price = product.default_price;
     return {
-      currench: price.currency,
+      currency: price.currency,
       id: product.id,
       name: product.name,
       price: price.unit_amount,
