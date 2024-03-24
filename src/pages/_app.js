fb677d410e1ba06f6d0360f3093d2859772cdc65
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-24 09:22:04 +0530

    Setup use-shopping-cart library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import AppLayout from "@/components/Layout";
 import "@/styles/globals.css";
+import { CartProvider } from "use-shopping-cart";
+
+const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <CartProvider cartMode="checkout-session" stripe={stripeKey} currency="INR">
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </CartProvider>
   );
 }
