6f2699ea39f7f2eaa30380f9305bd2739cdc18b7
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-24 20:50:29 +0530

    success /  Error Checkout page

---
+++ src/pages/success.js
@@ -0,0 +1,27 @@
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+
+export default function success() {
+  const data = false;
+  const error = true;
+  return (
+    <div className="container xl:max-w-screen-xl mx-auto py-12 px-6 text-center">
+      {error ? (
+        <div className="p-2 rounded-md bg-red-100 text-rose-500 max-w-md mx-auto">
+          <p className="text-lg">Sorry, Something went wrong</p>
+        </div>
+      ) : !data ? (
+        <div className="p-2 rounded-md text-gray-500 max-w-md mx-auto">
+          <p className="text-lg">Loading</p>
+        </div>
+      ) : (
+        <div className="py-4 px-8 space-y-4 rounded-md max-w-lg mx-auto">
+          <CheckCircleIcon className="w-24 h-24 mx-auto flex-shrink-0 text-lime-600" />
+          <h2 className="text-4xl font-semibold flex flex-col items-center space-x-1">
+            Thanks for your order
+          </h2>
+          <p className="text-lg">Check your email (email) for your invoice.</p>
+        </div>
+      )}
+    </div>
+  );
+}
